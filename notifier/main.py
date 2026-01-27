e4f383296b2108e22735c790487f38e922365a05
Ridwan Mulyadi <ridwan.mulyadi@live.com>
2026-01-28 06:51:22 +0700

    Support dismissing attention for multiple window classes, including "elecwhat".

--- notifier/main.py
+++ notifier/main.py
@@ -31,7 +31,7 @@ def send_attention():
             {
                 "id": ATTENTION_ID,
                 "command": ["toggle-window", "show", "wacli-tui"],
-                "dismiss_on_window_class": "wacli-tui",
+                "dismiss_on_window_classes": ["wacli-tui", "elecwhat"],
             }
         )
         sock.send(f"add_attention_by_cmd {payload}".encode())
